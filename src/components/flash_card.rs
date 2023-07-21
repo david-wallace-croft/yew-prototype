6c1a736f67aff8a3b44b83c0d4aeb39f07920beb
David Wallace Croft <david@CroftSoft.com>
2023-07-21 07:45:09 -0500

    button classes

--- src/components/flash_card.rs
+++ src/components/flash_card.rs
@@ -10,6 +10,7 @@ pub fn flash_card() -> Html {
   </button>
   <h1>{ "8 x 6 = ?" }</h1>
   <button
+    class={classes!("bg-blue-500", "hover:bg-blue-700", "text-white", "font-bold", "py-2", "px-4", "rounded")}
     type="button">
   {"16"}
   </button>
