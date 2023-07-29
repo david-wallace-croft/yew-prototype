847b489581ac38615e96da37cd0e96e13dce111b
David Wallace Croft <david@CroftSoft.com>
2023-07-29 10:51:43 -0500

    cargo fmt update

--- src/components/login.rs
+++ src/components/login.rs
@@ -1,3 +1,15 @@
+// =============================================================================
+//! - Login component for CroftSoft Yew Prototype
+//!
+//! # Metadata
+//! - Copyright: &copy; 2023 [`CroftSoft Inc`]
+//! - Author: [`David Wallace Croft`]
+//! - Updated: 2023-07-29
+//!
+//! [`CroftSoft Inc`]: https://www.croftsoft.com/
+//! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
+// =============================================================================
+
 use yew::prelude::*;
 
 #[function_component(Login)]
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,15 @@
+// =============================================================================
+//! - Components module for CroftSoft Yew Prototype
+//!
+//! # Metadata
+//! - Copyright: &copy; 2023 [`CroftSoft Inc`]
+//! - Author: [`David Wallace Croft`]
+//! - Updated: 2023-07-29
+//!
+//! [`CroftSoft Inc`]: https://www.croftsoft.com/
+//! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
+// =============================================================================
+
 pub mod app;
 pub mod flash_card;
 pub mod login;
